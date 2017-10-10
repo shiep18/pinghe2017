14c4258d7cae5b49a7edbd1e68e9894c7090bc79
Kainian Xie <1553730177@qq.com>
2017-10-10 12:03:18 +0800

    a simply python serial code

---
+++ serpiano/comtest.py
@@ -0,0 +1,34 @@
+import serial
+
+print 'hello'
+
+ser=serial.Serial(port='COM4')
+n=ser.write('1')
+
+print 'after write'
+print n
+
+def run():
+    action = "aaa"
+    while action != "q":
+        print 'select which tone do you want to play ? 1,2 q and others for quit'
+        action = raw_input("> ")
+        if action == "1":
+            ser.write('1')
+        elif action == "2":
+            ser.write('2')
+        else :
+            return
+
+run()
+
+
+
+
+
+    
+
+    
+
+
+
