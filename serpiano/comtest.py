7553dc06b80230f91b352d2817fa858029509fe0
xknxknqq <1553730177@qq.com>
2017-11-07 15:00:20 +0800

    clean up debug information

--- serpiano/comtest.py
+++ serpiano/comtest.py
@@ -8,7 +8,7 @@ ports = list(serial.tools.list_ports.comports())
 
 print (ports)
 
-song1 = ['1\n','1\n','5\n','5\n','6\n','6\n','5\n','4\n','4\n','3\n','3\n','2\n','2\n','1\n']
+song1 = ['1','1','5','5','6','6','5','5','4','4','3','3','2','2','1','1']
 song2 = ['1','2','3','1','1','2','3','1','3','4','5','3','4','5']
 
 for p in ports:
@@ -22,31 +22,15 @@ for p in ports:
 #ser=serial.Serial(port]='/dev/ttymodem542')
 
 def run():
-    action = "aaa"
+    action = "empty"
     while action != "q":
         print 'select which tone do you want to play ? 1,2 q and others for quit'
         action = raw_input("> ")
         if action == "1":
             for notes in song1:
-#                ser.write('1')
                 ser.write(notes)
-                ser.flushOutput
                 print "send:"+notes
-                echonotes="N"
-                notesint=1
-                echonotesint=1
-                while echonotesint != notesint:
-                    print "echonotes:"+echonotes
-                    print "notes:"+notes
-                    notesint=notes
-                    echonotesint=echonotes
-                    print "echonotesint:"+echonotesint
-                    print "notesint:"+notesint
-                    echonotes=ser.read()
-                print "get:"+echonotes
-#                time.sleep(1)
-                print "\n"
-                print "\n"
+                time.sleep(1)
         elif action == "2":
             ser.write('2')
         else :
