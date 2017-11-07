6ef7870dec10df4c9fb8a4908f66d5dec2b31266
xknxknqq <1553730177@qq.com>
2017-11-07 14:27:40 +0800

    serial search Arduino instead of CH340

--- serpiano/comtest.py
+++ serpiano/comtest.py
@@ -1,5 +1,6 @@
 import serial
 import serial.tools.list_ports
+import time
 
 print 'hello'
 
@@ -7,12 +8,12 @@ ports = list(serial.tools.list_ports.comports())
 
 print (ports)
 
-song1 = [1,1,5,5,6,6,5,4,4,3,3,2,2,1]
-song2 = [1,2,3,1,1,2,3,1,3,4,5,3,4,5]
+song1 = ['1\n','1\n','5\n','5\n','6\n','6\n','5\n','4\n','4\n','3\n','3\n','2\n','2\n','1\n']
+song2 = ['1','2','3','1','1','2','3','1','3','4','5','3','4','5']
 
 for p in ports:
     print (p[1])
-    if "CH340" in p[1]:
+    if "Arduino" in p[1]:
 	    ser = serial.Serial(port= p[0])
     else :
 	    print ("No Arduino Device was found connected to the computer")
@@ -26,7 +27,26 @@ def run():
         print 'select which tone do you want to play ? 1,2 q and others for quit'
         action = raw_input("> ")
         if action == "1":
-            ser.write('1')
+            for notes in song1:
+#                ser.write('1')
+                ser.write(notes)
+                ser.flushOutput
+                print "send:"+notes
+                echonotes="N"
+                notesint=1
+                echonotesint=1
+                while echonotesint != notesint:
+                    print "echonotes:"+echonotes
+                    print "notes:"+notes
+                    notesint=notes
+                    echonotesint=echonotes
+                    print "echonotesint:"+echonotesint
+                    print "notesint:"+notesint
+                    echonotes=ser.read()
+                print "get:"+echonotes
+#                time.sleep(1)
+                print "\n"
+                print "\n"
         elif action == "2":
             ser.write('2')
         else :
