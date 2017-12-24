b312796e541127fe9d35f3a040f9deba6fea8776
Kainian Xie <1553730177@qq.com>
2017-12-24 13:31:46 +0800

    comment out file code for qq

--- serpiano/comtest.py
+++ serpiano/comtest.py
@@ -9,21 +9,15 @@ print (ports)
 song1 = ['1','1','5','5','6','6','5','5','4','4','3','3','2','2','1','1']
 song2 = ['1','2','3','1','1','2','3','1','3','4','5','3','4','5']
 
-f = open('mysongs.csv', 'r')
-data = f.read()
-rows = data.split('\n')
-print(rows[0:5])
-row=rows[0]
-song1 = row.split(',')
+#f = open('mysongs.csv', 'r')
+#data = f.read()
+#rows = data.split('\n')
+#print(rows[0:5])
+#row=rows[0]
+#song1 = row.split(',')
 
-for p in ports:
-    print (p[1])
-    if "Arduino" in p[1]:
-	    ser=serial.Serial(port=p[0])
-    else :
-	    print ("No Arduino Device was found connected to the computer")
 
-#ser=serial.Serial(port='COM4')
+ser=serial.Serial(port='COM11')
 #ser=serial.Serial(port='/dev/ttymodem542')
 
 def run():
@@ -37,7 +31,10 @@ def run():
                 print ("send:"+notes)
                 time.sleep(1)
         elif action == "2":
-            ser.write('2'.encode())
+            for notes in song2:
+                ser.write('2'.encode())
+                print ("send:"+notes)
+                time.sleep(1)
 
         else :
             return
